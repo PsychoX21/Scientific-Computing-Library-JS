e933cc4ed92d0324e7e0012fb28fb32ec3be3938
Saksham Khandelwal <106555181+PsychoX21@users.noreply.github.com>
2025-02-16 00:35:24 +0530

    Update cfd.js

--- applications/cfd.js
+++ applications/cfd.js
@@ -1,7 +1,7 @@
 export class CFD {
     static navierStokes2D(grid, dt, viscosity, iterations, verbose = false) {
         let n = grid.length, m = grid[0].length;
-        let velocity = Array.from({ length: n }, () => Array(m).fill([0, 0]));
+        let velocity = Array.from({ length: n }, () => Array.from({ length: m }, () => [0, 0]));
 
         if (verbose) {
             console.log(`Starting 2D Navier-Stokes simulation...`);
@@ -12,26 +12,33 @@ export class CFD {
         }
 
         for (let iter = 0; iter < iterations; iter++) {
-            if (verbose) {
-                let progress = Math.floor((iter + 1) / iterations * 100);
-                if (progress % 5 === 0) {
-                    console.log(`Progress: ${progress}% (${iter + 1} / ${iterations} iterations)`);
-                }
-            }
+            let newVelocity = velocity.map(row => row.map(v => [...v]));
 
             for (let i = 1; i < n - 1; i++) {
                 for (let j = 1; j < m - 1; j++) {
                     let [u, v] = velocity[i][j];
-                    let laplacianU = (velocity[i - 1][j][0] + velocity[i + 1][j][0] + velocity[i][j - 1][0] + velocity[i][j + 1][0] - 4 * u) / (n * m);
-                    let laplacianV = (velocity[i - 1][j][1] + velocity[i + 1][j][1] + velocity[i][j - 1][1] + velocity[i][j + 1][1] - 4 * v) / (n * m);
-                    velocity[i][j] = [u + dt * (viscosity * laplacianU), v + dt * (viscosity * laplacianV)];
+
+                    let laplacianU = velocity[i - 1][j][0] + velocity[i + 1][j][0] + velocity[i][j - 1][0] + velocity[i][j + 1][0] - 4 * u;
+                    let laplacianV = velocity[i - 1][j][1] + velocity[i + 1][j][1] + velocity[i][j - 1][1] + velocity[i][j + 1][1] - 4 * v;
+
+                    newVelocity[i][j] = [
+                        u + dt * viscosity * laplacianU,
+                        v + dt * viscosity * laplacianV
+                    ];
                 }
             }
+
+            velocity = newVelocity;
+
+            if (verbose && iter % (iterations / 10) === 0) {
+                console.log(`Iteration ${iter + 1}/${iterations}`);
+            }
         }
+
         if (verbose) {
             console.log(`Simulation completed after ${iterations} iterations.`);
         }
-        
+
         return velocity;
     }
 }
