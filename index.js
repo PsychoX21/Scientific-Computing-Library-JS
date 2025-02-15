3671df3c2b50a18a77c44d8ed7e012b565c337dd
Saksham Khandelwal <106555181+PsychoX21@users.noreply.github.com>
2025-02-15 22:11:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { Differential } from './numerical/differential.js';
 
 import { MatrixOps } from './linear_algebra/matrix_operations.js';
 
-import { FFT } from './fft/fft.js';
+import { FFT, Complex } from './fft/fft.js';
 
 import { Bioinformatics } from './applications/bioinformatics.js';
 import { CFD } from './applications/cfd.js';
@@ -15,4 +15,4 @@ import { FFTApplications } from './applications/fft_application.js';
 
 import { Stats } from './statistics/stats.js';
 
-export { ODESolvers, RootFinding, LinearSolvers, Differential, MatrixOps, FFT, Bioinformatics, CFD, FEM, ODEApplications, FFTApplications, Stats };
+export { ODESolvers, RootFinding, LinearSolvers, Differential, MatrixOps, FFT, Complex, Bioinformatics, CFD, FEM, ODEApplications, FFTApplications, Stats };
